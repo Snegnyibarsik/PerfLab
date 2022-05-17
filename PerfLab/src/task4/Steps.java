5adc080a36e97257fb79a7687895549d96b06e15
snegn <snegn@DESKTOP-C06ECQR>
2022-05-17 12:58:56 +0300

    2022.05.17

---
+++ PerfLab/src/task4/Steps.java
@@ -0,0 +1,45 @@
+package task4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Steps {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		List <Integer> nums = new ArrayList<Integer>();
+		Scanner sc = new Scanner(new File(args[0]));
+		
+		do {
+		nums.add(sc.nextInt());
+		} while (sc.hasNext());
+			
+		List <Integer> dif = new ArrayList<Integer>();
+				
+		for (int j = 0; j<nums.size()-1; j++) {
+				dif.add((nums.get(j)-nums.get(j+1)));
+		}
+
+		int stepPlus = 0;
+		int stepMinus = 0;
+		
+		for (Integer i:dif) {
+			if (i<0) {
+				stepMinus = stepMinus +i;
+			}else {
+				stepPlus = stepPlus +i;
+			}
+		}
+		
+		if (stepPlus>Math.abs(stepMinus)) {
+			System.out.println(stepPlus);
+			}else {System.out.println(Math.abs(stepMinus));
+		}
+	
+
+	}
+
+}
